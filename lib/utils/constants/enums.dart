259f4ee24fac041875eb524a40297c3659b4c18a
Pedro <dsdias.pedro@gmail.com>
2025-01-30 10:48:32 -0300

    Enums constants

---
+++ lib/utils/constants/enums.dart
@@ -0,0 +1,10 @@
+//List of Enums
+//They cannot be created inside a class.
+
+/// Switch of Custom Brand-Text-Size Widget
+enum TextSizes { small, medium, large }
+
+enum OrderStatus { processing, shipped, delivered }
+
+enum PaymentMethods { paypal, googlePay, applePay, visa, masterCard, creditCard, paystack, razorPay, paytm }
+
