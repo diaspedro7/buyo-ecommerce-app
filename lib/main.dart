2594db87508779261f8e741b2dd40c17c39cf85a
Pedro <dsdias.pedro@gmail.com>
2025-01-30 22:41:06 -0300

    Splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:buyo_ecommerce_app/myapp.dart';
+import 'package:buyo_ecommerce_app/app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,7 +5,7 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:buyo_ecommerce_app/myapp.dart';
+import 'package:buyo_ecommerce_app/app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
